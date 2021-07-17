ca6b580b2a148927f72570ac6f7a73ee812c61d6
Ketrin <geleczkak26@gmail.com>
2021-07-17 22:11:03 +0300

    Add file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let camelCase = 0;
+let snake_case = 1;
+let PascalCase = 2;
+
+console.log(camelCase);
+alert(camelCase);
+console.log(snake_case);
+alert(snake_case);
+console.log(PascalCase);
+alert(PascalCase);
