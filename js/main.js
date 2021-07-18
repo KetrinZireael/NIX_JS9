c3d9937034fea247260a922b3ab0d30636cd77ca
Ketrin <geleczkak26@gmail.com>
2021-07-18 14:09:56 +0300

    Solution without function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-function array(myArray) {
-   let newArray = [];
-   let sum = 0;
+// function array(myArray) {
+//    let newArray = [];
+//    let sum = 0;
    
-   for(i = 0; i < myArray.lenght; i++) {
-        newArray.push(myArray[i] + sum);
-        console.log(newArray);
-   } 
+//    for(i = 0; i < myArray.lenght; i++) {
+//         newArray.push(myArray[i] + sum);
+//         console.log(newArray);
+//    } 
+// }
+
+let myArray = [1,2,3,4];
+let sum = 0;
+
+for (i = 0; i < myArray.length; i++) {
+   sum += myArray[i] ;
+   console.log(sum);
 }
